042b3f9bd72ba7920533f866bd78a4f92421e25e
Flora <flora.Frenot@stagiairesmns.fr>
2024-07-12 23:10:36 +0200

    Modification des routes

--- backend/routes/client.mjs
+++ backend/routes/client.mjs
@@ -1,19 +1,18 @@
 import express from 'express';
 import ctrl from '../controllers/client.mjs';
 const router = express.Router();
-// ici le / correspond à la route GET /bills/ (voir le index.mjs qui déclare l'import de ces routes avec app.use('/bills, billRoutes)
 router.get('/', ctrl.getItems)
 
-// ici la route GET /bills/:id
+// ici la route GET /clients/:id
 router.get('/:id', ctrl.getItem)
 
-// ici la route PATCH /bills/:id
+// ici la route PATCH /clients/:id
 router.patch('/:id', ctrl.patchItem)
 
-// ici la route POST /bills/
+// ici la route POST /clients/
 router.post('/', ctrl.postItem)
 
-// ici la route PATCH /bills/:id
+// ici la route PATCH /clients/:id
 router.delete('/:id', ctrl.deleteItem)
 
 export { router };
